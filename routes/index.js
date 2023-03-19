cc8eab66c29b2d6b0a02c82e19299736dc1a543d
davidmohan <admohan2002@gmail.com>
2023-03-19 15:29:05 +0530

    First

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sample Project' });
+  res.render('index', { title: 'Sample Express Project' });
 });
 
 module.exports = router;
